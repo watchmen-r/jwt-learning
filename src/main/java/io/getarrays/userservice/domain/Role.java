78456c745f51a50e863295c35ab236ddd0975201
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-07-03 22:58:08 +0900

    first commit

---
+++ src/main/java/io/getarrays/userservice/domain/Role.java
@@ -0,0 +1,16 @@
+package io.getarrays.userservice.domain;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package io.getarrays.userservice.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.repo.RoleRepo;
+import io.getarrays.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+    
+}
